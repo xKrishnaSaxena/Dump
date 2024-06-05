389bebe7862661650b541acded95d98dfc4072e2
xKrishnaSaxena <rio17105@gmail.com>
2024-06-06 01:55:08 +0530

    Cors updated and favicon changed

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -4,6 +4,21 @@ import cors = require("cors");
 import dotenv = require("dotenv");
 
 const app = express();
+const allowedOrigins = ["https://dump-vvfi.onrender.com"];
+app.use(
+  cors({
+    origin: function (origin, callback) {
+      // Allow requests with no origin, like mobile apps or curl requests
+      if (!origin) return callback(null, true);
+      if (allowedOrigins.indexOf(origin) === -1) {
+        const msg = `The CORS policy for this site does not allow access from the specified Origin.`;
+        return callback(new Error(msg), false);
+      }
+      return callback(null, true);
+    },
+    credentials: true, // Allow credentials if needed
+  })
+);
 
 dotenv.config({ path: "./config.env" });
 const port = process.env.PORT;
